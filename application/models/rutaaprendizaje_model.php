05526494507acf467b77d6e2a5fa7340df805f5a
Juan Camilo <xogost@gmail.com>
2013-03-08 09:56:11 -0500

    Estabilizacion de app en linux

--- application/models/rutaaprendizaje_model.php
+++ application/models/rutaaprendizaje_model.php
@@ -7,20 +7,20 @@ class RutaAprendizaje_model extends CI_Model {
 
 	function create($data)
 	{
-            $this->db->insert('rutaAprendizaje', $data);
+            $this->db->insert('rutaaprendizaje', $data);
             return $this->db->affected_rows();
 	}
         function update($data) {
-            $this->db->update('rutaAprendizaje', $data, array("id"=>$data["id"]));
+            $this->db->update('rutaaprendizaje', $data, array("id"=>$data["id"]));
             return $this->db->affected_rows();
         }
         function selectAll() {
-            $query = $this->db->get('rutaaPrendizaje');
+            $query = $this->db->get('rutaaprendizaje');
             return $query;
         }
         
         function selectOne($id) {
-            $query = $this->db->get_where('rutaPrendizaje', array("id" => $id));
+            $query = $this->db->get_where('rutaaprendizaje', array("id" => $id));
             return $query;
         }
 
