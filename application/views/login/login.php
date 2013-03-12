fdc8773ea8d59ac0eb32d67cde6cd42cc04f4df5
Juan Camilo <xogost@gmail.com>
2013-03-12 10:06:58 -0500

    Se agrega el ajuste de login para campo password

--- application/views/login/login.php
+++ application/views/login/login.php
@@ -2,7 +2,7 @@
     <h4>Nombre de usuario</h4>
     <input type="text" id="username" name="username" value=""/>
     <h4>Contraseña</h4>
-    <input type="text" id="password" name="password" value=""/>
+    <input type="password" id="password" name="password" value=""/>
     <br/>
     <input type="submit" value="Iniciar Sesión" class="btn-primary"/>
 </form>
