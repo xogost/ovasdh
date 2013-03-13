dbc984cf06b624f8a0a1a992b8f74c11a2eea624
Juan Camilo <xogost@gmail.com>
2013-03-13 10:34:23 -0500

    Configuracion

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/ovasdh';
+$config['base_url'] = 'http://localhost/ovasdh';
 
 /*
 |--------------------------------------------------------------------------
