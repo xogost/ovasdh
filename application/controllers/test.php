a661463beb858e62db05f4622170ade632dcb174
Juan Camilo <xogost@gmail.com>
2013-03-14 09:25:20 -0500

    Se colocan los radio button en la parte izquierda antes de la respuesta en el test

--- application/controllers/test.php
+++ application/controllers/test.php
@@ -67,46 +67,50 @@ class Test extends CI_Controller {
     }
 
     function getTestHtml() {
-        $id = $_GET["id"];
-        $this->load->model("Test_model", 'Test', true);
-        $this->load->model("Preguntas_model", 'Preguntas', true);
-        $this->load->model("Respuestas_model", 'Respuestas', true);
-        $this->load->model("Imagenes_model", 'Imagenes', true);
-        $testColleccion = $this->Test->selectOne($id);
-        $testItem = $testColleccion->result();
-        $valortest = $testItem[0]->valor;
-        $html = "<ol>";
-        $dataTest = $this->Preguntas->readTest($id);
-        foreach ($dataTest->result() as $itemPregunta) {
-            $textoPregunta = "<li><p>$itemPregunta->pregunta:</p>";
-            $respuestaCorrecta = $itemPregunta->respuestacorrecta;
-            $valorRespuestaCorrecta = $itemPregunta->valor;
-            $arrayImagenes = $this->Imagenes->read($itemPregunta->id);
-            foreach ($arrayImagenes->result() as $itemImagen) {
-                $textoPregunta = str_replace("[" . $itemImagen->nombre . "]", "<img width='70px' height='70px' class='img-polaroid' src='" . base_url("files") . "/$itemImagen->ruta'/>", $textoPregunta);
-            }
-            $html .= $textoPregunta;
-            $arrayRespuestas = $this->Respuestas->read($itemPregunta->id);
-            $html .= "<ol type='A' style='font-weight: bold;'>";
-            $consecutivoRespuesta = 1;
-            $cantidadPreguntas = 0;
-            foreach ($arrayRespuestas->result() as $itemRespuesta) {
-                if ($cantidadPreguntas <= 3) {
-                    $funcionJavaScriptSuma = "";
-                    if ($consecutivoRespuesta == $respuestaCorrecta)
-                        $funcionJavaScriptSuma = "onclick='sumar($valorRespuestaCorrecta);'";
-                    $html .= "<li><label for='radio$itemRespuesta->id'>$itemRespuesta->texto</label><input name='preg$itemPregunta->id' type='radio' $funcionJavaScriptSuma id='radio$itemRespuesta->id' value='$itemRespuesta->id'/></li>";
-                    $consecutivoRespuesta++;
+        try {
+            $id = $_GET["id"];
+            $this->load->model("Test_model", 'Test', true);
+            $this->load->model("Preguntas_model", 'Preguntas', true);
+            $this->load->model("Respuestas_model", 'Respuestas', true);
+            $this->load->model("Imagenes_model", 'Imagenes', true);
+            $testColleccion = $this->Test->selectOne($id);
+            $testItem = $testColleccion->result();
+            $valortest = $testItem[0]->valor;
+            $html = "<ol>";
+            $dataTest = $this->Preguntas->readTest($id);
+            foreach ($dataTest->result() as $itemPregunta) {
+                $textoPregunta = "<li><p>$itemPregunta->pregunta:</p>";
+                $respuestaCorrecta = $itemPregunta->respuestacorrecta;
+                $valorRespuestaCorrecta = $itemPregunta->valor;
+                $arrayImagenes = $this->Imagenes->read($itemPregunta->id);
+                foreach ($arrayImagenes->result() as $itemImagen) {
+                    $textoPregunta = str_replace("[" . $itemImagen->nombre . "]", "<img width='70px' height='70px' class='img-polaroid' src='" . base_url("files") . "/$itemImagen->ruta'/>", $textoPregunta);
                 }
-                $cantidadPreguntas++;
+                $html .= $textoPregunta;
+                $arrayRespuestas = $this->Respuestas->read($itemPregunta->id);
+                $html .= "<ol type='A' style='font-weight: bold;'>";
+                $consecutivoRespuesta = 1;
+                $cantidadPreguntas = 0;
+                foreach ($arrayRespuestas->result() as $itemRespuesta) {
+                    if ($cantidadPreguntas <= 3) {
+                        $funcionJavaScriptSuma = "";
+                        if ($consecutivoRespuesta == $respuestaCorrecta)
+                            $funcionJavaScriptSuma = "onclick='sumar($valorRespuestaCorrecta);'";
+                        $html .= "<li><input style='display: inline-block;' name='preg$itemPregunta->id' type='radio' $funcionJavaScriptSuma id='radio$itemRespuesta->id' value='$itemRespuesta->id'/><label style='display: inline-block; margin-left: 10px;' for='radio$itemRespuesta->id'>$itemRespuesta->texto</label></li>";
+                        $consecutivoRespuesta++;
+                    }
+                    $cantidadPreguntas++;
+                }
+                $html .= "</ol></li>";
             }
-            $html .= "</ol></li>";
+            $html .= "</ol><br/><input type='hidden' id='valtest' value='$valortest'/> <button class='btn btn-large btn-inverse' onclick=\"validarResultadoTest();\">Terminar Test</button>";
+            echo $html;
+        } catch (Exception $exc) {
+            echo "";
         }
-        $html .= "</ol><br/><input type='hidden' id='valtest' value='$valortest'/> <button class='btn btn-large btn-inverse' onclick=\"validarResultadoTest();\">Terminar Test</button>";
-        echo $html;
-        return;
     }
-    function saveResultTest(){
+
+    function saveResultTest() {
         $testid = $_POST["testid"];
         $intentos = $_POST["intentos"];
         $this->load->library('session');
@@ -114,10 +118,10 @@ class Test extends CI_Controller {
         $this->respuestas->insertResultadosTest($this->session->userdata("username"), $testid, $intentos);
         return true;
     }
-    
-    function eliminar(){
+
+    function eliminar() {
         $idtest = $_POST["idtest"];
-        $this->load->model("test_model","Test", True);
+        $this->load->model("test_model", "Test", True);
         $this->Test->delete($idtest);
         echo true;
     }
